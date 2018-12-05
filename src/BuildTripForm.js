106b8cfdefecedda96f8d6ccd5a7490a55ef9285
Foster <foster@fosterdyck.com>
2018-12-05 01:33:55 -0500

    added in select type functionality

--- src/BuildTripForm.js
+++ src/BuildTripForm.js
@@ -9,16 +9,17 @@ class BuildTripForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            selectedLocation: "",
             locationInput: "",
-            chooseLocation: "",
-            selectedType: "",
-            typeChoices: activitiesArray,
             locationData: [],
+            typeChoices: activitiesArray,
+            selectedType: "",
+            typeInput: "",
         }
     }
-    componentDidMount() {
+    // componentDidMount() {
         
-    }
+    // }
     handleChange = (e) => {
         this.setState({
             //target.value IS THE VALUE OF THE INPUT
@@ -31,14 +32,14 @@ class BuildTripForm extends Component {
         
         e.preventDefault();
         //This gives us a state value when the user clicks submit that we will use to pass on to the API and return with feedback.
-        const locationInput = this.state.chooseLocation;
+        const locationInput = this.state.selectedLocation;
         const apiKey = `YwiudiYi5fC5MKG0gh9W52CLVdfxeGhP`
         //STOPS EMPTY INPUTS
         if (locationInput !== '') {
-            // const locationInput = this.state.chooseLocation;
+            // const locationInput = this.state.selectedLocation;
             this.setState({
-                locationInput: locationInput,
-                chooseLocation: '',
+                locationInput,
+                selectedLocation: '',
             })
             //IF THE INPUT ISNT EMPTY FETCH THE API
             axios({
@@ -65,30 +66,42 @@ class BuildTripForm extends Component {
             })
         }
     }
-    // chooseType = (e) => {
-    //     e.preventDefault();
+    chooseType = (e) => {
+        e.preventDefault();
 
-    //     const selectType        
-    // }
+        const typeInput = this.state.selectedType;
+        //STOPS EMPTY INPUTS
+        if (typeInput !== '') {
+            // const locationInput = this.state.selectedLocation;
+            this.setState({
+                typeInput,
+            })        
+        }
+    }
     render() {
         return (
             <div className="BuildTripForm">
+                {/* I WILL BE PUTTING IN A CHANGE SO THAT ONE SMALLER BOX CAN CYCLE YOU THROUGH THE CHOICES SINCE WE WANT THEM TO BE MADETORY ANYWAY */}
                 <form className="tripForm locationForm" action="submit">
-                    <label htmlFor="chooseLocation" className="visuallyhidden">Input the location you wish to travel to.</label>
-                    <input type="text" name="chooseLocation" id="chooseLocation" placeholder="Enter location" onChange={this.handleChange}/>
+                    <label htmlFor="selectedLocation" className="visuallyhidden">Input the location you wish to travel to.</label>
+                    <input type="text" name="selectedLocation" id="selectedLocation" placeholder="Enter location" onChange={this.handleChange}/>
                     <input type="submit" value="Submit" onClick={this.selectLocationInput}/>
                 </form>
                 {/* I WILL BE PUTTING IN A CHANGE SO THAT ONE SMALLER BOX CAN CYCLE YOU THROUGH THE CHOICES SINCE WE WANT THEM TO BE MADETORY ANYWAY */}
                 <form className="tripForm typeForm" action="submit">
                     <label htmlFor="selectedType">Choose the type of trip you wish to take:</label>
-                    {/* HOW CAN I GET THIS TO START BLANK, THE FIRST INPUT IS NOT SENT TO THE STATE BUT IF I PUT A PLACEHOLDER IN THE AREA IT CAN BE A TYPE OF TRIP */}
-                    <select name="chooseType" id="chooseType" onChange={this.handleChange}>
+                    {/* HOW CAN I GET THIS TO START BLANK, THE FIRST INPUT IS NOT SENT TO THE STATE BUT IF I PUT A PLACEHOLDER IN THE AREA IT CAN BE A TYPE OF TRIP. HAVE IT NOT LETTING YOU SET STATE WITHOUT CHANGING BUT THAT DOESNT MAKE SENSE*/}
+                    <select name="selectedType" id="selectedType" onChange={this.handleChange}>
                         {this.state.typeChoices.map((type) => <option key={type} value={type}>{type}</option>)}
-                        <input type="submit" value="Submit"/>
                     </select>
+                        <input type="submit" value="Submit" onClick={this.chooseType}/>
                 </form>
                 {/* I WILL BE PUTTING IN A CHANGE SO THAT ONE SMALLER BOX CAN CYCLE YOU THROUGH THE CHOICES SINCE WE WANT THEM TO BE MADETORY ANYWAY */}
+                
+                {/* THIS FORM WILL LET US CHOOSE FROM THE STARTING CATAGORIES */}
 
+                {/* THIS FORM WILL LET US CHOOSE STARTING FRIENDS TO INVITE */}
+                
             </div>
         );
     }
