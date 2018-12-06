a00968e067137cc1c6ae307ad1b92200590c6dbb
mariamosmann <rossimosmann@gmail.com>
2018-12-06 17:39:30 -0500

    some pseudocode, some bases :airplane:

--- src/TripDetails.js
+++ src/TripDetails.js
@@ -4,6 +4,8 @@ import './App.css';
 // import Qs from 'qs';
 // import activitiesArray from './activitiesArray.js'
 
+//CHANGES: COUNTRY IS SET, USERS CAN ONLY ADD CITIES IN THAT COUNTRY, THEY DON'T NEED TO ADD A REASON ANYMORE, COUNTRY AND REASON (FROM DROP DOWN) SHOULD BE DISPLAYED, EVERY CITY THAT GETS APPROVED SHOULD GO TO AN ARRAY AND APPEAR IN A DROP DOWN WHEN THEY PRESS THE + BUTTON, EVERY BOARD WILL DISPLAY A NAME AND PLACE THAT IS RELATED TO (like HOTEL - PARIS), ON TOP OF THE PRE SET LIST THAT WILL BE DISPLAYED, USERS CAN PICK A CATHEGORY FOR THAT BOARD (hotel, activities, restaurantes, shows, etc -- so it will be easiet to implement suggestions later)
+
 //GROUP MEMBERS (side pannel)
 //members that were already added: use forEach method (we don't need another array, so no map or filter) on array of users to display their name and photo 
 //options for getting the array: as a prop, importing a js file (that will only have the array), making a call to the API and storing the info in the state of this component (I don't see any advantage in doing that. Information that needs to be passed around for other components should be in the App)
@@ -11,9 +13,7 @@ import './App.css';
 //make a button that will add more users
 //stretch goal in case we have a group admin: add an x icon so the admin can delete members
 
-//"HEADER"
-//use same structure as the dashboard, with logout option under the user's icon/photo, but instead of name of the user will display the name of the group (h2) (information passed as a prop from where we're storing information about the user's groups)
-//*****NOTE***** dashboard: info about the user (groups, friends, etc) || trip details: info about the trip itself
+
 
 //PICKING A PLACE
 //destination board (div)
@@ -24,31 +24,105 @@ import './App.css';
 //info will be added and displayed dynamically
 
 //VOTING SYSTEM +  ESSENTIALS BOARD
-//suggestions will have up and down icons with onClick to controle the voting system
+//suggestions will have up and down icons with onClick to control the voting system
 //everytime a user votes, it's id will be added to an array named as the place, it will serve as a way to count how many voted that place had (.length), as well as who voted for it 
 //when user tries to vote it will check if they already voted for that suggestions (filter? better method? +  if), they can vote in as many suggestions as they want, but only once
 //once a place reaches the majority of the votes (half the users + 1) take voting system out for that board, display all the essential boards, they will have the same structure as the destination board
 
 //ADDING BOARDS
+//when user clicks the + button a new board will be generated, like an essential board, with voting system
 
+//they can navigate between the boards using icons with onClick
 
+//FUNCTIONS SHOULD GO TO APP (as Adam mentioned)
+// !!!!! THESE FUNCTION SHOULD GO TO APP !!!!! 
 
+//Pop Up
+// function to make a pop up appear when a button is clicked
+// initial state should be:
+// popUpButton: false
+// this will be used to add user in TripDetails and open the new trip form in TripDashboard
 
+popUp = event => {
+    event.preventDefault();
 
-
-
-
+    if (this.state.popUpButton === false) {
+        this.setState({
+            popUpButton: true
+        })
+    } else if (this.state.popUpButton === true) {
+        this.setState({
+            popUpButton: false
+        })
+    }
+}
 
 class TripDetails extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-
+            
         }
     }
+
     render() {
         return (
-            <div className="App">
+            <div className="tripDetails">
+                {/* SITE MUST HAVE A GLOBAL HEADER TOO, router? */}
+
+                {/* TRIP DETAILS HEADER START */}
+                <header className="tripDetails__header header">
+                    {/* make country prop */}
+                    <h2 className="header__heading header__heading--h2">Trip to {this.props.country}</h2>
+
+                    {/* make type prop */}
+                    <h3 className="header__heading header__heading--h3">{this.props.type}</h3>
+                </header>
+
+                {/* GROUP START */}
+                <aside className="tripDetails__aside group">
+                    {//this prop should point to an array with this group members info
+                    this.props.groupMembers.forEach(member => {
+                        return (
+                            <div className="group__member">
+                                <div className="group__photoContainer">
+                                    <img src={member.photoURL} alt={`API_URL/${member.name}`} className="group__photo"/>
+                                </div>
+
+                                <p className="group__name">{member.name}</p>
+                            </div>
+                        )
+                    });
+                    }
+
+                    {/* make popUp prop to pass popUp function */}
+                    <button onClick={this.props.popUp} className="group__button">Add a Friend</button>
+
+                    {
+                    // make props for popUpButton
+                    this.state.popUpButton
+                    ? (
+                        <div className="popUp">
+                            <h3 className="popUp__heading">Add a Friend</h3>
+
+                            <form className="popUp__form" action="">
+                                <label htmlFor="addFriend" className="popUp__label">Your friend's email</label>
+                                <input type="email" id="addFriend" className="popUp__input" onChange={this.props.handleChange}required/>
+                            </form>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className="popUp popUp--hidden">                        
+                        </div>
+                    )
+                    }
+                    
+                </aside>
+
+                
+
+
 
             </div>
         );
@@ -56,3 +130,6 @@ class TripDetails extends Component {
 }
 
 export default TripDetails;
+
+//*****NOTES*****
+//main header should be the same for both group and details, with logout option under the user's icon/photo
